57032b9fd8c48f513be9380b4d2b9f3c0bc5d740
yderek5 <yderek5@gmail.com>
2017-10-31 22:43:05 -0500

    init repo

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+// TODOS
+// ***************************
+// Call sports api based on user input
+// Call twitter api depending on what team the user is viewing (if possible?)
+// Thursday: decide what to do with firebase
+// ***************************
